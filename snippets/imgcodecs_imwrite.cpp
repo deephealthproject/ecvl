c295cbabe2d4e4b748af9e77c5f2266115233547
Federico Bolelli <lk_number1@hotmail.it>
2019-05-16 18:08:25 +0200

    Add first snippet

---
+++ snippets/imgcodecs_imwrite.cpp
@@ -0,0 +1,25 @@
+#include "../src/filesystem.h"
+#include "../src/imgcodecs.h"
+
+using namespace std;
+using namespace ecvl;
+using namespace filesystem;
+
+int main()
+{
+    // Create BGR Image
+    Image img({ 500, 500, 3 }, DataType::uint8, "xyc", ColorType::BGR);
+    
+    // Populate Image with pseudo-random data
+    for (int r = 0; r < img.dims_[1]; ++r) {
+        for (int c = 0; c < img.dims_[0]; ++c) {
+            *img.Ptr({ c, r, 0 }) = 255;
+            *img.Ptr({ c, r, 1 }) = r % 255;
+            *img.Ptr({ c, r, 2 }) = c % 255;
+        }
+    }
+
+    ImWrite(path("./test.png"), img);
+
+    return EXIT_SUCCESS;
+}
