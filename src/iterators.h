2b0d3062933f34edd08b96fe17a90c7862f9d1bb
Federico Bolelli <lk_number1@hotmail.it>
2019-05-17 15:52:28 +0200

    Removed std::invoke

--- src/iterators.h
+++ src/iterators.h
@@ -18,7 +18,7 @@ struct Iterator {
     IncrementMemFn incrementor = &Iterator::IncrementPos;
 
     Iterator(Image& img, std::vector<int> pos = {});
-    Iterator& operator++() /* prefix */ { return std::invoke(incrementor, this); }
+    Iterator& operator++() /* prefix */ { return (this->*incrementor)(); }
     T& operator* () const { return *reinterpret_cast<T*>(ptr_); }
     T* operator-> () const { return reinterpret_cast<T*>(ptr_); }
     bool operator==(const Iterator& rhs) const { return ptr_ == rhs.ptr_; }
@@ -37,59 +37,15 @@ struct ConstIterator {
     typedef ConstIterator& (ConstIterator::*IncrementMemFn)();
     IncrementMemFn incrementor = &Iterator::IncrementPos;
 
-    explicit ConstIterator(const Image& img, std::vector<int> pos = {}) : img_{ &img }, pos_{ move(pos) }
-    {
-        if (img_->contiguous_)
-            incrementor = &ConstIterator::ContiguousIncrementPos;
-        if (pos_.empty()) { // Begin
-            pos_.resize(img_->dims_.size(), 0);
-            ptr_ = img.data_;
-        }
-        else {
-            if (pos_.size() != img_->dims_.size()) {
-                throw std::runtime_error("ConstIterator starting pos has a wrong size");
-            }
-            if (pos_ == img_->dims_) { // End 
-                if (img_->contiguous_) {
-                    ptr_ = img_->data_ + img_->datasize_;
-                }
-                else {
-                    ptr_ = nullptr;
-                }
-            }
-            else {
-                ptr_ = img_->Ptr(pos_);
-            }
-        }
-    }
-    ConstIterator& operator++() /* prefix */ { return std::invoke(incrementor, this); }
+    ConstIterator(const Image& img, std::vector<int> pos = {});
+    ConstIterator& operator++() /* prefix */ { return (this->*incrementor)(); }
     const T& operator* () const { return *reinterpret_cast<const T*>(ptr_); }
     const T* operator-> () const { return reinterpret_cast<const T*>(ptr_); }
     bool operator==(const ConstIterator& rhs) const { return ptr_ == rhs.ptr_; }
     bool operator!=(const ConstIterator& rhs) const { return ptr_ != rhs.ptr_; }
 private:
-    ConstIterator& IncrementPos()
-    {
-        int spos = pos_.size();
-        int dim;
-        for (dim = 0; dim < spos; ++dim) {
-            ++pos_[dim];
-            ptr_ += img_->strides_[dim];
-            if (pos_[dim] != img_->dims_[dim])
-                break;
-            // Back to dimension starting position
-            pos_[dim] = 0;
-            ptr_ -= img_->dims_[dim] * img_->strides_[dim];
-        }
-        if (dim == spos)
-            ptr_ = nullptr;
-        return *this;
-    }
-    ConstIterator& ContiguousIncrementPos()
-    {
-        ptr_ += img_->elemsize_;
-        return *this;
-    }
+    ConstIterator & IncrementPos();
+    ConstIterator& ContiguousIncrementPos();
 };
 
 
--- src/iterators_impl.h
+++ src/iterators_impl.h
@@ -25,6 +25,33 @@ Iterator<T>::Iterator(Image& img, std::vector<int> pos) : img_{ &img }, pos_{ mo
         }
     }
 }
+template <typename T>
+ConstIterator<T>::ConstIterator(const Image& img, std::vector<int> pos) : img_{ &img }, pos_{ move(pos) }
+{
+    if (img_->contiguous_)
+        incrementor = &ConstIterator::ContiguousIncrementPos;
+    if (pos_.empty()) { // Begin
+        pos_.resize(img_->dims_.size(), 0);
+        ptr_ = img.data_;
+    }
+    else {
+        if (pos_.size() != img_->dims_.size()) {
+            throw std::runtime_error("ConstIterator starting pos has a wrong size");
+        }
+        if (pos_ == img_->dims_) { // End 
+            if (img_->contiguous_) {
+                ptr_ = img_->data_ + img_->datasize_;
+            }
+            else {
+                ptr_ = nullptr;
+            }
+        }
+        else {
+            ptr_ = img_->Ptr(pos_);
+        }
+    }
+}
+
 
 template <typename T>
 Iterator<T>& Iterator<T>::IncrementPos()
@@ -44,9 +71,33 @@ Iterator<T>& Iterator<T>::IncrementPos()
         ptr_ = nullptr;
     return *this;
 }
+template <typename T>
+ConstIterator<T>& ConstIterator<T>::IncrementPos()
+{
+    int spos = pos_.size();
+    int dim;
+    for (dim = 0; dim < spos; ++dim) {
+        ++pos_[dim];
+        ptr_ += img_->strides_[dim];
+        if (pos_[dim] != img_->dims_[dim])
+            break;
+        // Back to dimension starting position
+        pos_[dim] = 0;
+        ptr_ -= img_->dims_[dim] * img_->strides_[dim];
+    }
+    if (dim == spos)
+        ptr_ = nullptr;
+    return *this;
+}
 
 template <typename T>
 Iterator<T>& Iterator<T>::ContiguousIncrementPos()
+{
+    ptr_ += img_->elemsize_;
+    return *this;
+}
+template <typename T>
+ConstIterator<T>& ConstIterator<T>::ContiguousIncrementPos()
 {
     ptr_ += img_->elemsize_;
     return *this;
