1093a25cd8dc242f8c6c0a67421f814a941dcf11
Federico Bolelli <lk_number1@hotmail.it>
2019-05-24 17:59:17 +0200

    Add snippet to test and debug natvis

---
+++ snippets/test_imagewatch.cpp
@@ -0,0 +1,25 @@
+#include "../src/core.h"
+#include <vector>
+using namespace ecvl;
+using namespace std;
+
+// This is not a real snippet but contains a bunch of images with 
+// different types to test the ImageWatch(natvis) functionalities
+
+int main()
+{
+    Image a({ 5, 5, 1 }, DataType::uint8, "xyc", ColorType::GRAY);
+    Image b({ 5, 5, 3 }, DataType::uint8, "xyc", ColorType::RGB);
+    Image c({ 5, 5, 3 }, DataType::uint8, "xyc", ColorType::BGR);
+
+    Image d({ 1, 5, 5 }, DataType::uint8, "cxy", ColorType::GRAY);
+    Image e({ 3, 5, 5 }, DataType::uint8, "cxy", ColorType::RGB);
+    Image f({ 3, 5, 5 }, DataType::uint8, "cxy", ColorType::BGR);
+
+    Image g({ 5, 5, 3 }, DataType::uint8, "xyz", ColorType::none);
+    Image h({ 5, 5 }, DataType::uint8, "xy", ColorType::none);
+    
+    vector<int> v = { 1,2,3 };
+    auto x = v[4];
+    return EXIT_SUCCESS;
+}
