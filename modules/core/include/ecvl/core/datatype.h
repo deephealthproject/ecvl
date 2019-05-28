492b8da9c27d41dd88863c3ef3e95da9a78a222d
Federico Bolelli <lk_number1@hotmail.it>
2019-05-28 15:28:30 +0200

    Add saturate_cast

--- modules/core/include/ecvl/core/datatype.h
+++ modules/core/include/ecvl/core/datatype.h
@@ -33,6 +33,62 @@ template<ecvl::DataType> struct TypeInfo { using basetype = void; };
 #include "datatype_tuples.inc"
 #undef ECVL_TUPLE
 
+
+
+/** @brief Saturate a value (of any type) to the specified type.
+
+Given an input of any type the saturate_cast function provide an
+output return value of the specified type applying saturation. When
+the input value in greater than the maximum possible value (max) for
+the output type, the max value is returned. When the input value in
+lower than the minimum possible value (min) for the output type, the
+min value is returned.
+
+@param[in] v Input value (of any type).
+
+@return Input value after cast and saturation.
+*/
+template<DataType ODT, typename IDT>
+typename TypeInfo<ODT>::basetype saturate_cast(IDT v) {
+    using basetype = typename TypeInfo<ODT>::basetype;
+
+    if (v > std::numeric_limits<basetype>::max()) {
+        return std::numeric_limits<basetype>::max();
+    }
+    if (v < std::numeric_limits<basetype>::min()) {
+        return std::numeric_limits<basetype>::min();
+    }
+
+    return static_cast<basetype>(v);
+}
+
+/** @brief Saturate a value (of any type) to the specified type.
+
+Given an input of any type the saturate_cast function provide an
+output return value of the specified type applying saturation. When
+the input value in greater than the maximum possible value (max) for
+the output type, the max value is returned. When the input value in
+lower than the minimum possible value (min) for the output type, the
+min value is returned.
+
+@param[in] v Input value (of any type).
+
+@return Input value after cast and saturation.
+*/
+template<typename ODT, typename IDT>
+ODT saturate_cast(const IDT& v) {
+
+    if (v > std::numeric_limits<ODT>::max()) {
+        return std::numeric_limits<ODT>::max();
+    }
+    if (v < std::numeric_limits<ODT>::min()) {
+        return std::numeric_limits<ODT>::min();
+    }
+
+    return static_cast<ODT>(v);
+}
+
+
 } // namespace ecvl
 
 #endif // ECVL_DATATYPE_H_
