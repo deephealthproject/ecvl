44485d27921287f716558e0fd307971a62320334
Federico Bolelli <lk_number1@hotmail.it>
2019-06-25 14:38:51 +0200

    Update doxygen latex generation

--- doc/doxygen/latex_doc_gen.bat
+++ doc/doxygen/latex_doc_gen.bat
@@ -1,4 +1,10 @@
+copy Doxyfile Doxyfile_LaTeX
+echo GENERATE_LATEX=YES >> Doxyfile_LaTeX
 doxygen Doxyfile_LaTeX
 copy doxygen.sty ..\latex\doxygen.sty /Y
 call ..\latex\make.bat
 copy ..\latex\refman.pdf .\refman.pdf /Y
+del /f Doxyfile_LaTeX
+del /f refman_final.pdf
+call pdftk refman.pdf cat 3-end output refman_final.pdf
+del /f refman.pdf
