9b6f6288dbfae791e7125d844be0751efce349ba
Laura Canalini <laura.canalini@unimore.it>
2019-07-31 18:16:46 +0200

    Update ECVL EDDL interface

--- modules/eddl/include/ecvl/eddl.h
+++ modules/eddl/include/ecvl/eddl.h
@@ -5,16 +5,51 @@
 #include "ecvl/core/image.h"
 
 namespace ecvl {
+    /** @brief Convert an EDDL Tensor into an ECVL Image.
 
-    /** @brief Convert a EDDL Tensor into an ECVL Image.
+    Tensor dimensions must be \f$C\f$ x \f$Y\f$ x \f$X\f$ or \f$Z\f$ x \f$C\f$ x \f$Y\f$ x \f$X\f$, where: \n
+    \f$Z\f$ = depth \n
+    \f$C\f$ = color channels \n
+    \f$Y\f$ = height \n
+    \f$X\f$ = width
 
     @param[in] t Input EDDL Tensor.
+    @param[in] c_type ecvl::ColorType of input data (optional). \n
+    If c_type is ColorType::none (default), it is assumed that: \n
+    If the input has 4 channels, the color type is assumed to be ColorType::RGBA. \n
+    If the input has 3 channels, the color type is assumed to be ColorType::BGR. \n
+    If the input has 1 channels, the color type is assumed to be ColorType::GRAY. \n
+    In any other case, the color type is assumed to be ColorType::none.
 
     @return ECVL Image.
     */
-    Image TensorToImage(tensor& t);
+    Image TensorToImage(tensor& t, ColorType c_type = ColorType::none);
 
-    /** @brief Convert an ECVL Image into EDDL Tensor.
+    /** @brief Convert an EDDL Tensor into an ECVL View.
+
+    Tensor dimensions must be \f$C\f$ x \f$Y\f$ x \f$X\f$ or \f$Z\f$ x \f$C\f$ x \f$Y\f$ x \f$X\f$, where: \n
+    \f$Z\f$ = depth \n
+    \f$C\f$ = color channels \n
+    \f$Y\f$ = height \n
+    \f$X\f$ = width
+
+    @param[in] t Input EDDL Tensor.
+    @param[in] c_type ecvl::ColorType of input data (optional). \n
+    If c_type is ColorType::none (default), it is assumed that: \n
+    If the input has 4 channels, the color type is assumed to be ColorType::RGBA. \n
+    If the input has 3 channels, the color type is assumed to be ColorType::BGR. \n
+    If the input has 1 channels, the color type is assumed to be ColorType::GRAY. \n
+    In any other case, the color type is assumed to be ColorType::none.
+
+    @return ECVL View.
+    */
+    View<DataType::float32> TensorToView(tensor& t, ColorType c_type = ColorType::none);
+
+    /** @brief Convert an ECVL Image into an EDDL Tensor.
+
+    Image must have 3 or 4 dimensions. \n
+    If the Image has 3 dimensions, the output Tensor will be created with shape \f$C\f$ x \f$Y\f$ x \f$X\f$. \n
+    If the Image has 4 dimensions, the output Tensor will be created with shape \f$Z\f$ x \f$C\f$ x \f$Y\f$ x \f$X\f$.
 
     @param[in] img Input ECVL Image.
 
@@ -25,13 +60,13 @@ namespace ecvl {
     /** @brief Convert a set of images into a single EDDL Tensor.
 
     @param[in] dataset Vector of all images path.
-    @param[in] dims Dimensions of the dataset in the form: number of total images, number of channels of the images, 
-    and the width and height at which all the images has to be resized. \f$\{number\_of\_samples, number\_of\_channels, width, height\}\f$
+    @param[in] dims Dimensions of the dataset in the form: number of total images, number of color channels of the images,
+    and the width and height at which all the images has to be resized. Output Tensor will have these dimensions. \n
+    \f$\{number\_of\_samples, number\_of\_colors\_channels, width, height\}\f$
 
     @return EDDL Tensor.
     */
-    typedef void(*AugFunctions)(Image&);
-    tensor DatasetToTensor(vector<string> dataset, const std::vector<int>& dims, vector<AugFunctions>aug_f = {});
+    tensor DatasetToTensor(vector<string> dataset, const std::vector<int>& dims);
 
 } // namespace ecvl 
 
--- modules/eddl/src/support_eddl.cpp
+++ modules/eddl/src/support_eddl.cpp
@@ -2,72 +2,163 @@
 
 #include "ecvl/core/imgproc.h"
 #include "ecvl/core/imgcodecs.h"
+#include "ecvl/core/standard_errors.h"
 
-#include <functional>
 #include <iostream>
-#include <cmath>
+
+using namespace eddl;
 
 namespace ecvl
 {
-    Image TensorToImage(tensor& t)
+    void SetColorType(ColorType& c_type, const int &color_channels)
     {
-        if (t->data->ndim == 2)
+        if (c_type == ColorType::none)
         {
-            Image img({ (int)sqrt(t->data->shape[1]), (int)sqrt(t->data->shape[1]), 1 }, DataType::float32, "xyc", ColorType::GRAY);
-            memcpy(img.data_, t->input->ptr, img.datasize_);
-            return img;
+            switch (color_channels)
+            {
+            case 1:     c_type = ColorType::GRAY;      break;
+            case 3:     c_type = ColorType::BGR;       break;
+            case 4:     c_type = ColorType::RGBA;      break;
+            default:
+                c_type = ColorType::none;
+            }
         }
-        else
+    }
+
+    Image TensorToImage(tensor& t, ColorType c_type)
+    {
+        if (t->data->ndim != 3 && t->data->ndim != 4)
         {
-            Image img({ t->input->shape }, DataType::float32, "xyc", ColorType::BGR);
-            memcpy(img.data_, t->input->ptr, img.datasize_);
-            return img;
+            ECVL_ERROR_MSG "Tensor dims must be C x H x W or N x C x H x W";
         }
+
+        Image img;
+
+        switch (t->data->ndim)
+        {
+        case 3:
+            if (c_type == ColorType::none)
+                SetColorType(c_type, t->data->shape[0]);
+            img.Create({ t->data->shape[2], t->data->shape[1], t->data->shape[0] }, DataType::float32, "xyc", c_type);
+            break;
+        case 4:
+            if (c_type == ColorType::none)
+                SetColorType(c_type, t->data->shape[1]);
+            img.Create({ t->data->shape[3], t->data->shape[2], t->data->shape[0], t->data->shape[1] }, DataType::float32, "xyzc", c_type);
+            break;
+        default:
+            ECVL_ERROR_NOT_REACHABLE_CODE
+        }
+
+        memcpy(img.data_, t->data->ptr, img.datasize_);
+
+        return img;
+    }
+
+    View<DataType::float32> TensorToView(tensor& t, ColorType c_type)
+    {
+        if (t->data->ndim != 3 && t->data->ndim != 4)
+        {
+            ECVL_ERROR_MSG "Tensor dims must be C x H x W or N x C x H x W";
+        }
+
+        View<DataType::float32> v;
+
+        switch (t->data->ndim)
+        {
+        case 3:
+            if (c_type == ColorType::none)
+                SetColorType(c_type, t->data->shape[0]);
+            v.dims_.push_back(t->data->shape[2]);
+            v.dims_.push_back(t->data->shape[1]);
+            v.dims_.push_back(t->data->shape[0]);
+            v.channels_ = "xyc";
+            break;
+        case 4:
+            if (c_type == ColorType::none)
+                SetColorType(c_type, t->data->shape[1]);
+            v.dims_.push_back(t->data->shape[3]);
+            v.dims_.push_back(t->data->shape[2]);
+            v.dims_.push_back(t->data->shape[0]);
+            v.dims_.push_back(t->data->shape[1]);
+            v.channels_ = "xyzc";
+            break;
+        default:
+            ECVL_ERROR_NOT_REACHABLE_CODE
+        }
+
+        v.colortype_ = c_type;
+        v.data_ = (uint8_t*)t->data->ptr;
+        v.elemtype_ = DataType::float32;
+        v.elemsize_ = DataTypeSize(DataType::float32);
+        v.spacings_ = {};
+        v.contiguous_ = true;
+        v.meta_ = { nullptr };
+        v.mem_ = ShallowMemoryManager::GetInstance();
+
+        // Compute strides
+        v.strides_ = { v.elemsize_ };
+        int dsize = v.dims_.size();
+        for (int i = 0; i < dsize - 1; ++i) {
+            v.strides_.push_back(v.strides_[i] * v.dims_[i]);
+        }
+
+        // Compute datasize
+        v.datasize_ = v.elemsize_;
+        v.datasize_ = std::accumulate(begin(v.dims_), end(v.dims_), v.datasize_, std::multiplies<size_t>());
+
+        return v;
     }
 
     tensor ImageToTensor(const Image& img)
     {
-        Image tmp;
+        if (img.dims_.size() != 3 && img.dims_.size() != 4)
+        {
+            ECVL_ERROR_MSG "Image must have 3 or 4 dimensions";
+        }
+
+        Image tmp; tensor t;
         CopyImage(img, tmp, DataType::float32);
 
-        if (tmp.channels_ != "xyc")
-            RearrangeChannels(tmp, tmp, "xyc");
+        switch (tmp.dims_.size())
+        {
+        case 3:
+            if (tmp.channels_ != "xyc")
+                RearrangeChannels(tmp, tmp, "xyc");
+            t = T({ tmp.dims_[2], tmp.dims_[1], tmp.dims_[0] });
+            break;
+        case 4:
+            if (tmp.channels_ != "xyzc")
+                RearrangeChannels(tmp, tmp, "xyzc");
+            t = T({ tmp.dims_[2], tmp.dims_[3], tmp.dims_[1], tmp.dims_[0] });
+            break;
+        default:
+            ECVL_ERROR_NOT_REACHABLE_CODE
+        }
 
-        int tot_dims = tmp.dims_[0] * tmp.dims_[1] * tmp.dims_[2];
-        tensor t = T({ tot_dims });
-        memcpy(t->input->ptr, tmp.data_, tmp.datasize_);
+        memcpy(t->data->ptr, tmp.data_, tmp.datasize_);
         return t;
     }
 
-    tensor DatasetToTensor(vector<string> dataset, const std::vector<int>& dims, vector<AugFunctions>aug_f)
+    tensor DatasetToTensor(vector<string> dataset, const std::vector<int>& dims)
     {
-        tensor t; Image img_tmp;
+        if (dims.size() != 4)
+            ECVL_ERROR_MSG "dims must have 4 dimensions (n_samples, color_channels, height, width )";
 
-        //dims[0] = n_samples, dims[1] = n_channels, dims[2] = width, dims[3] = height
-        int tot_dims = dims[1] * dims[2] * dims[3];
-        tensor stack = T({ dims[0], tot_dims });
+        Image tmp;
+        tensor t; 
         int i = 0;
-        cv::TickMeter tm;
 
-        tm.reset();
-        tm.start();
+        tensor stack = T({ dims });
+
         for (auto& elem : dataset) {
-            ImRead(elem, img_tmp);
-            ResizeDim(img_tmp, img_tmp, { dims[2], dims[3] });
-
-            //data augmentation
-            for (auto f = aug_f.begin(); f != aug_f.end(); ++f)
-                (*f)(img_tmp);
-
-            t = ImageToTensor(img_tmp);
-            memcpy(stack->input->ptr + t->input->size * i, t->input->ptr, t->input->size * sizeof(float));
+            ImRead(elem, tmp);
+            ResizeDim(tmp, tmp, { dims[3], dims[2] });
+            t = ImageToTensor(tmp);
+            memcpy(stack->data->ptr + t->data->size * i, t->data->ptr, t->data->size * sizeof(float));
             ++i;
-            if (i % 100 == 0)
-                std::cout << i << " images processed" << endl;
         }
 
-        tm.stop();
-        std::cout << "Elapsed " << tm.getTimeSec() << " s\n";
         return stack;
     }
 }
