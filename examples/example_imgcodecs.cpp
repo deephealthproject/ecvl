f0f42f9d4a11824763d260143c07b266bdaf7021
MicheleCancilla <cancilla.michele@gmail.com>
2019-10-28 15:18:40 +0100

    Add ECVL_BUILD_EXAMPLES CMake option and allow CMake to download the images needed for examples

---
+++ examples/example_imgcodecs.cpp
@@ -0,0 +1,23 @@
+#include "ecvl/core.h"
+
+using namespace std;
+using namespace ecvl;
+
+int main()
+{
+    // Create BGR Image
+    Image img({ 500, 500, 3 }, DataType::uint8, "xyc", ColorType::BGR);
+
+    // Populate Image with pseudo-random data
+    for (int r = 0; r < img.dims_[1]; ++r) {
+        for (int c = 0; c < img.dims_[0]; ++c) {
+            *img.Ptr({ c, r, 0 }) = 255;
+            *img.Ptr({ c, r, 1 }) = (r / 2) % 255;
+            *img.Ptr({ c, r, 2 }) = (r / 2) % 255;
+        }
+    }
+
+    ImWrite("./test.png", img);
+
+    return EXIT_SUCCESS;
+}
