74a892a70408e1d520960640ecce361a906f78b7
Michele Cancilla <cancilla.michele@gmail.com>
2020-04-22 18:50:04 +0200

    Fix imgproc tests

--- modules/core/test/test_imgproc.cpp
+++ modules/core/test/test_imgproc.cpp
@@ -80,8 +80,9 @@ TEST_F(Imgproc, Threshold##type) \
 TEST_F(Imgproc, ThresholdSameDst##type) \
 { \
     Threshold(g2_##type, g2_##type, 35, 127, ThresholdingType::BINARY); \
-    EXPECT_TRUE(g2_##type##_v({ 0,0,0 }) == 127); EXPECT_TRUE(g2_##type##_v({ 1,0,0 }) == 0); \
-    EXPECT_TRUE(g2_##type##_v({ 0,1,0 }) == 0); EXPECT_TRUE(g2_##type##_v({ 1,1,0 }) == 127); \
+    View<DataType::type> out_v(g2_##type); \
+    EXPECT_TRUE(out_v({ 0,0,0 }) == 127); EXPECT_TRUE(out_v({ 1,0,0 }) == 0); \
+    EXPECT_TRUE(out_v({ 0,1,0 }) == 0); EXPECT_TRUE(out_v({ 1,1,0 }) == 127); \
 } \
 \
 TEST_F(Imgproc, Mirror2D##type) \
@@ -108,19 +109,22 @@ TEST_F(Imgproc, Mirror2D##type) \
 TEST_F(Imgproc, Mirror2DSameDst##type) \
 { \
     Mirror2D(g1_##type, g1_##type); \
-    EXPECT_TRUE(g1_##type##_v({ 0,0,0 }) == 50); \
+    View<DataType::type> out_v(g1_##type); \
+    EXPECT_TRUE(out_v({ 0,0,0 }) == 50); \
     \
     Mirror2D(g2_##type, g2_##type); \
-    EXPECT_TRUE(g2_##type##_v({ 0,0,0 }) == 32); EXPECT_TRUE(g2_##type##_v({ 1,0,0 }) == 50); \
-    EXPECT_TRUE(g2_##type##_v({ 0,1,0 }) == 60); EXPECT_TRUE(g2_##type##_v({ 1,1,0 }) == 14); \
+    out_v = g2_##type; \
+    EXPECT_TRUE(out_v({ 0,0,0 }) == 32); EXPECT_TRUE(out_v({ 1,0,0 }) == 50); \
+    EXPECT_TRUE(out_v({ 0,1,0 }) == 60); EXPECT_TRUE(out_v({ 1,1,0 }) == 14); \
     \
     Mirror2D(rgb2_##type, rgb2_##type); \
-    EXPECT_TRUE(rgb2_##type##_v({ 0,0,0 }) == 32); EXPECT_TRUE(rgb2_##type##_v({ 1,0,0 }) == 50); \
-    EXPECT_TRUE(rgb2_##type##_v({ 0,1,0 }) == 60); EXPECT_TRUE(rgb2_##type##_v({ 1,1,0 }) == 14); \
-    EXPECT_TRUE(rgb2_##type##_v({ 0,0,1 }) == 32); EXPECT_TRUE(rgb2_##type##_v({ 1,0,1 }) == 50); \
-    EXPECT_TRUE(rgb2_##type##_v({ 0,1,1 }) == 60); EXPECT_TRUE(rgb2_##type##_v({ 1,1,1 }) == 14); \
-    EXPECT_TRUE(rgb2_##type##_v({ 0,0,2 }) == 32); EXPECT_TRUE(rgb2_##type##_v({ 1,0,2 }) == 50); \
-    EXPECT_TRUE(rgb2_##type##_v({ 0,1,2 }) == 60); EXPECT_TRUE(rgb2_##type##_v({ 1,1,2 }) == 14); \
+    out_v = rgb2_##type; \
+    EXPECT_TRUE(out_v({ 0,0,0 }) == 32); EXPECT_TRUE(out_v({ 1,0,0 }) == 50); \
+    EXPECT_TRUE(out_v({ 0,1,0 }) == 60); EXPECT_TRUE(out_v({ 1,1,0 }) == 14); \
+    EXPECT_TRUE(out_v({ 0,0,1 }) == 32); EXPECT_TRUE(out_v({ 1,0,1 }) == 50); \
+    EXPECT_TRUE(out_v({ 0,1,1 }) == 60); EXPECT_TRUE(out_v({ 1,1,1 }) == 14); \
+    EXPECT_TRUE(out_v({ 0,0,2 }) == 32); EXPECT_TRUE(out_v({ 1,0,2 }) == 50); \
+    EXPECT_TRUE(out_v({ 0,1,2 }) == 60); EXPECT_TRUE(out_v({ 1,1,2 }) == 14); \
 } \
 \
 TEST_F(Imgproc, Flip2D##type) \
@@ -147,19 +151,22 @@ TEST_F(Imgproc, Flip2D##type) \
 TEST_F(Imgproc, Flip2DSameDst##type) \
 { \
     Flip2D(g1_##type, g1_##type); \
-    EXPECT_TRUE(g1_##type##_v({ 0,0,0 }) == 50); \
+    View<DataType::type> out_v(g1_##type); \
+    EXPECT_TRUE(out_v({ 0,0,0 }) == 50); \
     \
     Flip2D(g2_##type, g2_##type); \
-    EXPECT_TRUE(g2_##type##_v({ 0,0,0 }) == 14); EXPECT_TRUE(g2_##type##_v({ 1,0,0 }) == 60); \
-    EXPECT_TRUE(g2_##type##_v({ 0,1,0 }) == 50); EXPECT_TRUE(g2_##type##_v({ 1,1,0 }) == 32); \
+    out_v = g2_##type; \
+    EXPECT_TRUE(out_v({ 0,0,0 }) == 14); EXPECT_TRUE(out_v({ 1,0,0 }) == 60); \
+    EXPECT_TRUE(out_v({ 0,1,0 }) == 50); EXPECT_TRUE(out_v({ 1,1,0 }) == 32); \
     \
     Flip2D(rgb2_##type, rgb2_##type); \
-    EXPECT_TRUE(g2_##type##_v({ 0,0,0 }) == 14); EXPECT_TRUE(g2_##type##_v({ 1,0,0 }) == 60); \
-    EXPECT_TRUE(g2_##type##_v({ 0,1,0 }) == 50); EXPECT_TRUE(g2_##type##_v({ 1,1,0 }) == 32); \
-    EXPECT_TRUE(g2_##type##_v({ 0,0,1 }) == 14); EXPECT_TRUE(g2_##type##_v({ 1,0,1 }) == 60); \
-    EXPECT_TRUE(g2_##type##_v({ 0,1,1 }) == 50); EXPECT_TRUE(g2_##type##_v({ 1,1,1 }) == 32); \
-    EXPECT_TRUE(g2_##type##_v({ 0,0,2 }) == 14); EXPECT_TRUE(g2_##type##_v({ 1,0,2 }) == 60); \
-    EXPECT_TRUE(g2_##type##_v({ 0,1,2 }) == 50); EXPECT_TRUE(g2_##type##_v({ 1,1,2 }) == 32); \
+    out_v = rgb2_##type; \
+    EXPECT_TRUE(out_v({ 0,0,0 }) == 14); EXPECT_TRUE(out_v({ 1,0,0 }) == 60); \
+    EXPECT_TRUE(out_v({ 0,1,0 }) == 50); EXPECT_TRUE(out_v({ 1,1,0 }) == 32); \
+    EXPECT_TRUE(out_v({ 0,0,1 }) == 14); EXPECT_TRUE(out_v({ 1,0,1 }) == 60); \
+    EXPECT_TRUE(out_v({ 0,1,1 }) == 50); EXPECT_TRUE(out_v({ 1,1,1 }) == 32); \
+    EXPECT_TRUE(out_v({ 0,0,2 }) == 14); EXPECT_TRUE(out_v({ 1,0,2 }) == 60); \
+    EXPECT_TRUE(out_v({ 0,1,2 }) == 50); EXPECT_TRUE(out_v({ 1,1,2 }) == 32); \
 } \
 \
 TEST_F(Imgproc, HConcat##type) \
@@ -186,19 +193,22 @@ TEST_F(Imgproc, HConcat##type) \
 TEST_F(Imgproc, HConcatSameDst##type) \
 { \
     HConcat({ g1_##type, g1_##type }, g1_##type); \
-    EXPECT_TRUE(g1_##type##_v({ 0,0,0 }) == 50); EXPECT_TRUE(g1_##type##_v({ 1,0,0 }) == 50); \
+    View<DataType::type> out_v(g1_##type); \
+    EXPECT_TRUE(out_v({ 0,0,0 }) == 50); EXPECT_TRUE(out_v({ 1,0,0 }) == 50); \
     \
     HConcat({ g2_##type, g2_##type }, g2_##type); \
-    EXPECT_TRUE(g2_##type##_v({ 0,0,0 }) == 50); EXPECT_TRUE(g2_##type##_v({ 1,0,0 }) = 32); EXPECT_TRUE(g2_##type##_v({ 2,0,0 }) == 50); EXPECT_TRUE(g2_##type##_v({ 3,0,0 }) = 32); \
-    EXPECT_TRUE(g2_##type##_v({ 0,1,0 }) == 14); EXPECT_TRUE(g2_##type##_v({ 1,1,0 }) = 60); EXPECT_TRUE(g2_##type##_v({ 2,1,0 }) == 14); EXPECT_TRUE(g2_##type##_v({ 3,1,0 }) = 60); \
+    out_v = g2_##type; \
+    EXPECT_TRUE(out_v({ 0,0,0 }) == 50); EXPECT_TRUE(out_v({ 1,0,0 }) = 32); EXPECT_TRUE(out_v({ 2,0,0 }) == 50); EXPECT_TRUE(out_v({ 3,0,0 }) = 32); \
+    EXPECT_TRUE(out_v({ 0,1,0 }) == 14); EXPECT_TRUE(out_v({ 1,1,0 }) = 60); EXPECT_TRUE(out_v({ 2,1,0 }) == 14); EXPECT_TRUE(out_v({ 3,1,0 }) = 60); \
     \
     HConcat({ rgb2_##type, rgb2_##type }, rgb2_##type); \
-    EXPECT_TRUE(rgb2_##type##_v({ 0,0,0 }) == 50); EXPECT_TRUE(rgb2_##type##_v({ 1,0,0 }) = 32); EXPECT_TRUE(rgb2_##type##_v({ 2,0,0 }) == 50); EXPECT_TRUE(rgb2_##type##_v({ 3,0,0 }) = 32); \
-    EXPECT_TRUE(rgb2_##type##_v({ 0,1,0 }) == 14); EXPECT_TRUE(rgb2_##type##_v({ 1,1,0 }) = 60); EXPECT_TRUE(rgb2_##type##_v({ 2,1,0 }) == 14); EXPECT_TRUE(rgb2_##type##_v({ 3,1,0 }) = 60); \
-    EXPECT_TRUE(rgb2_##type##_v({ 0,0,1 }) == 50); EXPECT_TRUE(rgb2_##type##_v({ 1,0,1 }) = 32); EXPECT_TRUE(rgb2_##type##_v({ 2,0,1 }) == 50); EXPECT_TRUE(rgb2_##type##_v({ 3,0,1 }) = 32); \
-    EXPECT_TRUE(rgb2_##type##_v({ 0,1,1 }) == 14); EXPECT_TRUE(rgb2_##type##_v({ 1,1,1 }) = 60); EXPECT_TRUE(rgb2_##type##_v({ 2,1,1 }) == 14); EXPECT_TRUE(rgb2_##type##_v({ 3,1,1 }) = 60); \
-    EXPECT_TRUE(rgb2_##type##_v({ 0,0,2 }) == 50); EXPECT_TRUE(rgb2_##type##_v({ 1,0,2 }) = 32); EXPECT_TRUE(rgb2_##type##_v({ 2,0,2 }) == 50); EXPECT_TRUE(rgb2_##type##_v({ 3,0,2 }) = 32); \
-    EXPECT_TRUE(rgb2_##type##_v({ 0,1,2 }) == 14); EXPECT_TRUE(rgb2_##type##_v({ 1,1,2 }) = 60); EXPECT_TRUE(rgb2_##type##_v({ 2,1,2 }) == 14); EXPECT_TRUE(rgb2_##type##_v({ 3,1,2 }) = 60); \
+    out_v = rgb2_##type; \
+    EXPECT_TRUE(out_v({ 0,0,0 }) == 50); EXPECT_TRUE(out_v({ 1,0,0 }) = 32); EXPECT_TRUE(out_v({ 2,0,0 }) == 50); EXPECT_TRUE(out_v({ 3,0,0 }) = 32); \
+    EXPECT_TRUE(out_v({ 0,1,0 }) == 14); EXPECT_TRUE(out_v({ 1,1,0 }) = 60); EXPECT_TRUE(out_v({ 2,1,0 }) == 14); EXPECT_TRUE(out_v({ 3,1,0 }) = 60); \
+    EXPECT_TRUE(out_v({ 0,0,1 }) == 50); EXPECT_TRUE(out_v({ 1,0,1 }) = 32); EXPECT_TRUE(out_v({ 2,0,1 }) == 50); EXPECT_TRUE(out_v({ 3,0,1 }) = 32); \
+    EXPECT_TRUE(out_v({ 0,1,1 }) == 14); EXPECT_TRUE(out_v({ 1,1,1 }) = 60); EXPECT_TRUE(out_v({ 2,1,1 }) == 14); EXPECT_TRUE(out_v({ 3,1,1 }) = 60); \
+    EXPECT_TRUE(out_v({ 0,0,2 }) == 50); EXPECT_TRUE(out_v({ 1,0,2 }) = 32); EXPECT_TRUE(out_v({ 2,0,2 }) == 50); EXPECT_TRUE(out_v({ 3,0,2 }) = 32); \
+    EXPECT_TRUE(out_v({ 0,1,2 }) == 14); EXPECT_TRUE(out_v({ 1,1,2 }) = 60); EXPECT_TRUE(out_v({ 2,1,2 }) == 14); EXPECT_TRUE(out_v({ 3,1,2 }) = 60); \
 } \
 \
 TEST_F(Imgproc, VConcat##type) \
@@ -233,44 +243,47 @@ TEST_F(Imgproc, VConcat##type) \
 \
 TEST_F(Imgproc, VConcatSameDst##type) \
 { \
-    VConcat({ g1_##type, g1_##type },  g1_##type); \
-    EXPECT_TRUE(g1_##type##_v({ 0,0,0 }) == 50); \
-    EXPECT_TRUE(g1_##type##_v({ 0,1,0 }) == 50); \
-    \
-    VConcat({ g2_##type, g2_##type }, g2_##type); \
-    EXPECT_TRUE(g2_##type##_v({ 0,0,0 }) == 50); EXPECT_TRUE(g2_##type##_v({ 1,0,0 }) = 32); \
-    EXPECT_TRUE(g2_##type##_v({ 0,1,0 }) == 14); EXPECT_TRUE(g2_##type##_v({ 1,1,0 }) = 60); \
-    EXPECT_TRUE(g2_##type##_v({ 0,2,0 }) == 50); EXPECT_TRUE(g2_##type##_v({ 1,2,0 }) = 32); \
-    EXPECT_TRUE(g2_##type##_v({ 0,3,0 }) == 14); EXPECT_TRUE(g2_##type##_v({ 1,3,0 }) = 60); \
-    \
-    VConcat({ rgb2_##type, rgb2_##type },  rgb2_##type); \
-    EXPECT_TRUE(rgb2_##type##_v({ 0,0,0 }) == 50); EXPECT_TRUE(rgb2_##type##_v({ 1,0,0 }) = 32); \
-    EXPECT_TRUE(rgb2_##type##_v({ 0,1,0 }) == 14); EXPECT_TRUE(rgb2_##type##_v({ 1,1,0 }) = 60); \
-    EXPECT_TRUE(rgb2_##type##_v({ 0,2,0 }) == 50); EXPECT_TRUE(rgb2_##type##_v({ 1,2,0 }) = 32); \
-    EXPECT_TRUE(rgb2_##type##_v({ 0,3,0 }) == 14); EXPECT_TRUE(rgb2_##type##_v({ 1,3,0 }) = 60); \
-    EXPECT_TRUE(rgb2_##type##_v({ 0,0,1 }) == 50); EXPECT_TRUE(rgb2_##type##_v({ 1,0,1 }) = 32); \
-    EXPECT_TRUE(rgb2_##type##_v({ 0,1,1 }) == 14); EXPECT_TRUE(rgb2_##type##_v({ 1,1,1 }) = 60); \
-    EXPECT_TRUE(rgb2_##type##_v({ 0,2,1 }) == 50); EXPECT_TRUE(rgb2_##type##_v({ 1,2,1 }) = 32); \
-    EXPECT_TRUE(rgb2_##type##_v({ 0,3,1 }) == 14); EXPECT_TRUE(rgb2_##type##_v({ 1,3,1 }) = 60); \
-    EXPECT_TRUE(rgb2_##type##_v({ 0,0,2 }) == 50); EXPECT_TRUE(rgb2_##type##_v({ 1,0,2 }) = 32); \
-    EXPECT_TRUE(rgb2_##type##_v({ 0,1,2 }) == 14); EXPECT_TRUE(rgb2_##type##_v({ 1,1,2 }) = 60); \
-    EXPECT_TRUE(rgb2_##type##_v({ 0,2,2 }) == 50); EXPECT_TRUE(rgb2_##type##_v({ 1,2,2 }) = 32); \
-    EXPECT_TRUE(rgb2_##type##_v({ 0,3,2 }) == 14); EXPECT_TRUE(rgb2_##type##_v({ 1,3,2 }) = 60); \
-} \
-\
-TEST_F(Imgproc, ResizeDim##type) \
-{ \
-    ResizeDim(g2_uint8, out, { 1, 1 }, InterpolationType::nearest); \
-    View<DataType::uint8> out_v(out); \
-    EXPECT_THAT(out.dims_, testing::ElementsAre(1, 1, 1)); \
+    VConcat({ g1_##type, g1_##type }, g1_##type); \
+    View<DataType::type> out_v(g1_##type); \
     EXPECT_TRUE(out_v({ 0,0,0 }) == 50); \
+    EXPECT_TRUE(out_v({ 0,1,0 }) == 50); \
     \
-    ResizeDim(g2_uint8, out, { 2, 1 }, InterpolationType::nearest); \
-    out_v = out; \
-    EXPECT_THAT(out.dims_, testing::ElementsAre(2, 1, 1)); \
-    EXPECT_TRUE(out_v({ 0,0,0 }) == 50); EXPECT_TRUE(out_v({ 1,0,0 }) == 32); \
+    VConcat({ g2_##type, g2_##type },  g2_##type); \
+    out_v =  g2_##type; \
+    EXPECT_TRUE(out_v({ 0,0,0 }) == 50); EXPECT_TRUE(out_v({ 1,0,0 }) = 32); \
+    EXPECT_TRUE(out_v({ 0,1,0 }) == 14); EXPECT_TRUE(out_v({ 1,1,0 }) = 60); \
+    EXPECT_TRUE(out_v({ 0,2,0 }) == 50); EXPECT_TRUE(out_v({ 1,2,0 }) = 32); \
+    EXPECT_TRUE(out_v({ 0,3,0 }) == 14); EXPECT_TRUE(out_v({ 1,3,0 }) = 60); \
+    \
+    VConcat({ rgb2_##type, rgb2_##type }, rgb2_##type); \
+    out_v = rgb2_##type; \
+    EXPECT_TRUE(out_v({ 0,0,0 }) == 50); EXPECT_TRUE(out_v({ 1,0,0 }) = 32); \
+    EXPECT_TRUE(out_v({ 0,1,0 }) == 14); EXPECT_TRUE(out_v({ 1,1,0 }) = 60); \
+    EXPECT_TRUE(out_v({ 0,2,0 }) == 50); EXPECT_TRUE(out_v({ 1,2,0 }) = 32); \
+    EXPECT_TRUE(out_v({ 0,3,0 }) == 14); EXPECT_TRUE(out_v({ 1,3,0 }) = 60); \
+    EXPECT_TRUE(out_v({ 0,0,1 }) == 50); EXPECT_TRUE(out_v({ 1,0,1 }) = 32); \
+    EXPECT_TRUE(out_v({ 0,1,1 }) == 14); EXPECT_TRUE(out_v({ 1,1,1 }) = 60); \
+    EXPECT_TRUE(out_v({ 0,2,1 }) == 50); EXPECT_TRUE(out_v({ 1,2,1 }) = 32); \
+    EXPECT_TRUE(out_v({ 0,3,1 }) == 14); EXPECT_TRUE(out_v({ 1,3,1 }) = 60); \
+    EXPECT_TRUE(out_v({ 0,0,2 }) == 50); EXPECT_TRUE(out_v({ 1,0,2 }) = 32); \
+    EXPECT_TRUE(out_v({ 0,1,2 }) == 14); EXPECT_TRUE(out_v({ 1,1,2 }) = 60); \
+    EXPECT_TRUE(out_v({ 0,2,2 }) == 50); EXPECT_TRUE(out_v({ 1,2,2 }) = 32); \
+    EXPECT_TRUE(out_v({ 0,3,2 }) == 14); EXPECT_TRUE(out_v({ 1,3,2 }) = 60); \
 }
 
 #include "ecvl/core/datatype_existing_tuples.inc.h"
 #undef ECVL_TUPLE
+
+TEST_F(Imgproc, ResizeDimuint8) 
+{ 
+    ResizeDim(g1_uint8, out, { 1, 1 }, InterpolationType::nearest); 
+    View<DataType::uint8> out_v(out);
+    EXPECT_THAT(out.dims_, testing::ElementsAre(1, 1, 1)); 
+    EXPECT_TRUE(out_v({ 0,0,0 }) == 50); 
+    
+    ResizeDim(g2_uint8, out, { 2, 1 }, InterpolationType::nearest); 
+    out_v = out; 
+    EXPECT_THAT(out.dims_, testing::ElementsAre(2, 1, 1)); 
+    EXPECT_TRUE(out_v({ 0,0,0 }) == 50); EXPECT_TRUE(out_v({ 1,0,0 }) == 32); 
+}
 }
