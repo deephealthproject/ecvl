c9017e3d76b2650d4f0b38c324b2a1b297d819f8
DeepHealthJenkins <50112386+DeepHealthJenkins@users.noreply.github.com>
2020-05-20 19:23:34 +0200

    Update docs

--- master/search/all_10.js
+++ master/search/all_10.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['query',['Query',['../classecvl_1_1_meta_data.html#a5f12fad6548dfa7da9920957e68a5d70',1,'ecvl::MetaData']]]
+  ['query',['Query',['../classecvl_1_1_meta_data.html#a5f12fad6548dfa7da9920957e68a5d70',1,'ecvl::MetaData::Query()'],['../classecvl_1_1_overlay_meta_data.html#a03ef0f3558b4c0c255b8fe38b34f34ec',1,'ecvl::OverlayMetaData::Query()']]]
 ];
