4d35ca172bfd4283b7e3313b3406292b57ed689f
DeepHealthJenkins <50112386+DeepHealthJenkins@users.noreply.github.com>
2020-11-13 13:04:39 +0100

    Update docs

--- master/search/typedefs_7.js
+++ master/search/typedefs_7.js
@@ -1,4 +1,7 @@
 var searchData=
 [
+  ['scalar',['Scalar',['../namespaceecvl.html#a95a1d037461340fa937aeb2560f7b99c',1,'ecvl']]],
+  ['size2d',['Size2d',['../namespaceecvl.html#a00abc3fd61d4ee6716fffa54eba18bbd',1,'ecvl']]],
+  ['size2i',['Size2i',['../namespaceecvl.html#a0f0e49de5db4641babfd40ec4750f5e3',1,'ecvl']]],
   ['superior_5ftype',['superior_type',['../structecvl_1_1promote__superior__type.html#aaf192f08dcc4c7e96dd122a7b51ba610',1,'ecvl::promote_superior_type']]]
 ];
