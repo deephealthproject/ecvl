8787a04bf120626ac3e1c01203309e84cf397fcd
Michele Cancilla <cancilla.michele@gmail.com>
2021-01-26 12:44:53 +0100

    Update license header

--- modules/eddl/include/ecvl/augmentations.h
+++ modules/eddl/include/ecvl/augmentations.h
@@ -1,7 +1,7 @@
 /*
 * ECVL - European Computer Vision Library
 * Version: 0.3.1
-* copyright (c) 2020, Università degli Studi di Modena e Reggio Emilia (UNIMORE), AImageLab
+* copyright (c) 2021, Università degli Studi di Modena e Reggio Emilia (UNIMORE), AImageLab
 * Authors:
 *    Costantino Grana (costantino.grana@unimore.it)
 *    Federico Bolelli (federico.bolelli@unimore.it)
