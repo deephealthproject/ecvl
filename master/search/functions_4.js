2a5982a8728a8e51bdb2c472db8b430081a25036
DeepHealthJenkins <50112386+DeepHealthJenkins@users.noreply.github.com>
2021-06-10 11:56:31 +0200

    Update docs

--- master/search/functions_4.js
+++ master/search/functions_4.js
@@ -9,7 +9,7 @@ var searchData=
   ['dicomread',['DicomRead',['../namespaceecvl.html#a5a24c2fd23ca0b2ff9932639670cb462',1,'ecvl']]],
   ['dicomwrite',['DicomWrite',['../namespaceecvl.html#a320e10aa5a8503df04214d12f2870adf',1,'ecvl']]],
   ['div',['Div',['../classecvl_1_1_image.html#a5544bb3d5832a16484b5f7089f2dc1ea',1,'ecvl::Image::Div()'],['../namespaceecvl.html#a8c09fc3fc39394bcb58aef741e8e00e0',1,'ecvl::Div(const Image &amp;src1, const Image &amp;src2, Image &amp;dst, DataType dst_type=DataType::none, bool saturate=true)'],['../namespaceecvl.html#a1a6fe418821045fd2ffbd5eeca5a1c14',1,'ecvl::Div(const Image &amp;src1, const ST2 &amp;src2, Image &amp;dst, DataType dst_type=DataType::none, bool saturate=true)'],['../namespaceecvl.html#a989916c6663dba724713dbc73ffc1fcf',1,'ecvl::Div(const ST1 &amp;src1, const Image &amp;src2, Image &amp;dst, DataType dst_type=DataType::none, bool saturate=true)']]],
-  ['dldataset',['DLDataset',['../classecvl_1_1_d_l_dataset.html#a3e087c5f79cf0e00432b2676836de763',1,'ecvl::DLDataset']]],
+  ['dldataset',['DLDataset',['../classecvl_1_1_d_l_dataset.html#a0603aa06e966074ac2026454c578ef72',1,'ecvl::DLDataset']]],
   ['drawellipse',['DrawEllipse',['../namespaceecvl.html#a8afc71baadb6e9e0f1f3605c909a42db',1,'ecvl']]],
   ['dropcolorchannel',['DropColorChannel',['../namespaceecvl.html#a49f122a730aee0ec33a0e28287142ede',1,'ecvl']]],
   ['dump',['Dump',['../classecvl_1_1_dataset.html#a54d235d4382c3349e9a3ea9a9155842d',1,'ecvl::Dataset']]]
