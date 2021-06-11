2f2087265bc8acbdaf8149dd02f7b955b94e0757
DeepHealthJenkins <50112386+DeepHealthJenkins@users.noreply.github.com>
2021-06-11 11:26:42 +0200

    Update docs

--- master/search/functions_16.js
+++ master/search/functions_16.js
@@ -2,7 +2,6 @@ var searchData=
 [
   ['_7eaugmentation',['~Augmentation',['../classecvl_1_1_augmentation.html#a9c6cbd9201bcec9416bcd2a8b5969e52',1,'ecvl::Augmentation']]],
   ['_7edataset',['~Dataset',['../classecvl_1_1_dataset.html#aa9d49dd324c195b3669c368d0d287304',1,'ecvl::Dataset']]],
-  ['_7edldataset',['~DLDataset',['../classecvl_1_1_d_l_dataset.html#a3d1a9a809235888872391b247e3aa1ac',1,'ecvl::DLDataset']]],
   ['_7egeneratedataset',['~GenerateDataset',['../classecvl_1_1_generate_dataset.html#aef36f813d42d1d6d4e00ee3e7bcdc5be',1,'ecvl::GenerateDataset']]],
   ['_7eimage',['~Image',['../classecvl_1_1_image.html#a2230eb993f310b23cf7624db0d42ee7c',1,'ecvl::Image']]],
   ['_7einitdcmtk',['~InitDCMTK',['../structecvl_1_1_init_d_c_m_t_k.html#a50a3651e81f487260980fa73969bb0b3',1,'ecvl::InitDCMTK']]],
