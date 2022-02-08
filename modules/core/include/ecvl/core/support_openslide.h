1149dda91067483a74e1ffea920bee41cf5d088b
Laura Canalini <laura.canalini@unimore.it>
2022-02-08 10:58:32 +0100

    Update version to 1.0.1

--- modules/core/include/ecvl/core/support_openslide.h
+++ modules/core/include/ecvl/core/support_openslide.h
@@ -1,6 +1,6 @@
 /*
 * ECVL - European Computer Vision Library
-* Version: 1.0.0
+* Version: 1.0.1
 * copyright (c) 2021, Università degli Studi di Modena e Reggio Emilia (UNIMORE), AImageLab
 * Authors:
 *    Costantino Grana (costantino.grana@unimore.it)
