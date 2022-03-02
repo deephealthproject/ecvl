5aa2785f4ea56efee8918f7b31ca704a2078f851
Laura Canalini <laura.canalini@unimore.it>
2022-03-02 15:20:57 +0100

    Update to version 1.0.2

--- modules/core/src/arithmetic.cpp
+++ modules/core/src/arithmetic.cpp
@@ -1,6 +1,6 @@
 /*
 * ECVL - European Computer Vision Library
-* Version: 1.0.1
+* Version: 1.0.2
 * copyright (c) 2021, Università degli Studi di Modena e Reggio Emilia (UNIMORE), AImageLab
 * Authors:
 *    Costantino Grana (costantino.grana@unimore.it)
